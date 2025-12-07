c41f6a0f08777c1ab701f1f70092224b83068d9f
Josue Vasquez <vasquezponcejosuedaniel@gmail.com>
2025-12-06 20:57:22 -0600

    Cambios Principales

--- js/loadcomponents.js
+++ js/loadcomponents.js
@@ -1,11 +1,20 @@
-fetch("componentes/navbar.html")
-    .then(response => response.text())
-    .then(html =>{
-        document.getElementById("navbar-container").innerHTML = html;
-    });
-
 fetch("componentes/footer.html")
     .then(response => response.text())
     .then(html =>{
         document.getElementById("footer-container").innerHTML = html;
     });
+
+
+const phonemq=window.matchMedia("(max-width: 400px)")
+let menuOp=document.getElementsByClassName("menu")
+
+
+menuOp[0].addEventListener("click",(e)=>{
+    console.log("holis")
+})
+
+phonemq.addEventListener("change",(e)=>{
+    menuOp[0].hidden="false"
+})
+
+
