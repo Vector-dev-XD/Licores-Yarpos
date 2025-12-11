5f6c8d937582461c3dfd354294908e4ee9738d79
Josue Vasquez <vasquezponcejosuedaniel@gmail.com>
2025-12-10 18:07:28 -0600

    Animaciones y responsividad arreglada, sidebar funcional y navbar simplificado

--- js/loadcomponents.js
+++ js/loadcomponents.js
@@ -1,3 +1,10 @@
+fetch("componentes/navbar.html")
+    .then(response => response.text())
+    .then(html =>{
+        document.getElementById("navbar-container").innerHTML = html;
+    });
+
+
 fetch("componentes/footer.html")
     .then(response => response.text())
     .then(html =>{
